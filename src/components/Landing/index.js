6faee69983f25e337c76acb2c4b7c90bc93d6cdb
C-Rodg <C-Rodg@users.noreply.github.com>
2021-05-22 18:45:02 -0700

    Added switch for landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,6 +1,25 @@
-// TODO: check if already authenticated and go to home if so, otherwise go to login.
-function LandingPage() {
-	return <div className="landing-page">LandingPage</div>;
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+class LandingPage extends Component {
+	componentDidMount() {
+		this.listener = this.props.firebase.auth.onAuthStateChanged((authUser) => {
+			console.log(authUser);
+			authUser
+				? this.props.history.push(ROUTES.HOME)
+				: this.props.history.push(ROUTES.SIGN_IN);
+		});
+	}
+
+	componentWillUnmount() {
+		this.listener();
+	}
+	render() {
+		return <div className="landing-page">Loading...</div>;
+	}
 }
 
-export default LandingPage;
+export default withRouter(withFirebase(LandingPage));
