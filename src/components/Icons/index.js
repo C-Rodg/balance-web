a205f9ddd6d24052e5e0866c0f569d0cab60d12f
cerebrium <nicholas.m.shankland@gmail.com>
2021-06-07 23:05:27 -0700

    removed the large import, it is faster, still need to solve the dynamic

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {material} from 'styled-icons'
+import {Commute, Business, Book, Theaters, SportsEsports, Cake, Hiking, OutdoorGrill, SportsScore, SportsFootball, SportsGolf, SportsBasketball, Flight, Hotel, ShoppingCart, Pets, Agriculture, HomeWork, Home, PestControl, AcUnit, Plumbing, PersonalVideo, Healing } from '@styled-icons/material'
 import PropTypes from 'prop-types'
 import './style.css';
 
@@ -22,10 +22,10 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.Commute size={size} title="Commute" onClick={(e) => selectIcon(e, 'Commute')}/>
+                        <Commute size={size} title="Commute" onClick={(e) => selectIcon(e, 'Commute')}/>
                     </div>
                     <div className="icon">
-                        <material.Business size={size} title="Bussiness Expense" onClick={(e) => selectIcon(e, 'Business')}/>
+                        <Business size={size} title="Bussiness Expense" onClick={(e) => selectIcon(e, 'Business')}/>
                     </div>
                 </div>
             </div>
@@ -37,34 +37,34 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.Book size={size} title="Book" onClick={(e) => selectIcon(e, 'Book')}/>
+                        <Book size={size} title="Book" onClick={(e) => selectIcon(e, 'Book')}/>
                     </div>
                     <div className="icon">
-                        <material.Theaters size={size} title="Theaters" onClick={(e) => selectIcon(e, 'Theaters')}/>
+                        <Theaters size={size} title="Theaters" onClick={(e) => selectIcon(e, 'Theaters')}/>
                     </div>
                     <div className="icon">
-                        <material.SportsEsports size={size} title="Computer" onClick={(e) => selectIcon(e, 'SportsEsports')}/>
+                        <SportsEsports size={size} title="Computer" onClick={(e) => selectIcon(e, 'SportsEsports')}/>
                     </div>
                     <div className="icon">
-                        <material.Cake size={size} title="Birthday" onClick={(e) => selectIcon(e, 'Cake')}/>
+                        <Cake size={size} title="Birthday" onClick={(e) => selectIcon(e, 'Cake')}/>
                     </div>
                     <div className="icon">
-                        <material.Hiking size={size} title="Hiking" onClick={(e) => selectIcon(e, 'Hiking')}/>
+                        <Hiking size={size} title="Hiking" onClick={(e) => selectIcon(e, 'Hiking')}/>
                     </div>
                     <div className="icon">
-                        <material.OutdoorGrill size={size} title="Grilling" onClick={(e) => selectIcon(e, 'OutdoorGrill')}/>
+                        <OutdoorGrill size={size} title="Grilling" onClick={(e) => selectIcon(e, 'OutdoorGrill')}/>
                     </div>
                     <div className="icon">
-                        <material.SportsScore size={size} title="General Sports" onClick={(e) => selectIcon(e, 'SportsScore')}/>
+                        <SportsScore size={size} title="General Sports" onClick={(e) => selectIcon(e, 'SportsScore')}/>
                     </div>
                     <div className="icon">
-                        <material.SportsFootball size={size} title="Football" onClick={(e) => selectIcon(e, 'SportsFootball')}/>
+                        <SportsFootball size={size} title="Football" onClick={(e) => selectIcon(e, 'SportsFootball')}/>
                     </div>
                     <div className="icon">
-                        <material.SportsGolf size={size} title="Golf" onClick={(e) => selectIcon(e, 'SportsGolf')}/>
+                        <SportsGolf size={size} title="Golf" onClick={(e) => selectIcon(e, 'SportsGolf')}/>
                     </div>
                     <div className="icon">
-                        <material.SportsBasketball size={size} title="Basketball" onClick={(e) => selectIcon(e, 'SportsBasketball')}/>
+                        <SportsBasketball size={size} title="Basketball" onClick={(e) => selectIcon(e, 'SportsBasketball')}/>
                     </div>
                 </div>
             </div>
@@ -75,10 +75,10 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.Flight size={size} title="Flights" onClick={(e) => selectIcon(e, 'Flight')}/>
+                        <Flight size={size} title="Flights" onClick={(e) => selectIcon(e, 'Flight')}/>
                     </div>
                     <div className="icon">
-                        <material.Hotel size={size} title="Hotel" onClick={(e) => selectIcon(e, 'Hotel')}/>
+                        <Hotel size={size} title="Hotel" onClick={(e) => selectIcon(e, 'Hotel')}/>
                     </div>
 
                 </div>
@@ -90,13 +90,13 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.ShoppingCart size={size} title="Groceries" onClick={(e) => selectIcon(e, 'ShoppingCart')}/>
+                        <ShoppingCart size={size} title="Groceries" onClick={(e) => selectIcon(e, 'ShoppingCart')}/>
                     </div>
                     <div className="icon">
-                        <material.Pets size={size} title="Pets" onClick={(e) => selectIcon(e, 'Pets')}/>
+                        <Pets size={size} title="Pets" onClick={(e) => selectIcon(e, 'Pets')}/>
                     </div>
                     <div className="icon">
-                        <material.Agriculture size={size} title="Vegetables" onClick={(e) => selectIcon(e, 'Agriculture')}/>
+                        <Agriculture size={size} title="Vegetables" onClick={(e) => selectIcon(e, 'Agriculture')}/>
                     </div>
                 </div>
             </div>
@@ -108,19 +108,19 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.HomeWork size={size} title="Homework" onClick={(e) => selectIcon(e, 'HomeWork')}/>
+                        <HomeWork size={size} title="Homework" onClick={(e) => selectIcon(e, 'HomeWork')}/>
                     </div>
                     <div className="icon">
-                        <material.Home size={size} title="Rent" onClick={(e) => selectIcon(e, 'Home')}/>
+                        <Home size={size} title="Rent" onClick={(e) => selectIcon(e, 'Home')}/>
                     </div>
                     <div className="icon">
-                        <material.PestControl size={size} title="Pest Control" onClick={(e) => selectIcon(e, 'PestControl')}/>
+                        <PestControl size={size} title="Pest Control" onClick={(e) => selectIcon(e, 'PestControl')}/>
                     </div>
                     <div className="icon">
-                        <material.AcUnit size={size} title="AC" onClick={(e) => selectIcon(e, 'AcUnit')}/>
+                        <AcUnit size={size} title="AC" onClick={(e) => selectIcon(e, 'AcUnit')}/>
                     </div>
                     <div className="icon">
-                        <material.Plumbing size={size} title="Water" onClick={(e) => selectIcon(e, 'Plumbing')}/>
+                        <Plumbing size={size} title="Water" onClick={(e) => selectIcon(e, 'Plumbing')}/>
                     </div>
                 </div>
             </div>
@@ -131,10 +131,10 @@ function Icons (props) {
                 </div>
                 <div className="iconsRow">
                     <div className="icon">
-                        <material.PersonalVideo size={size} title="Camera" onClick={(e) => selectIcon(e, 'PersonalVideo')}/>
+                        <PersonalVideo size={size} title="Camera" onClick={(e) => selectIcon(e, 'PersonalVideo')}/>
                     </div>
                     <div className="icon">
-                        <material.Healing size={size} title="Medical Expenses" onClick={(e) => selectIcon(e, 'Healing')}/>
+                        <Healing size={size} title="Medical Expenses" onClick={(e) => selectIcon(e, 'Healing')}/>
                     </div>
                 </div>
             </div>
--- src/components/Icons/individualIcon.js
+++ src/components/Icons/individualIcon.js
@@ -1,66 +1,83 @@
 import React, {lazy, Suspense, useEffect} from 'react';
-import {material} from 'styled-icons';
+import {Commute, Business, Book, Theaters, SportsEsports, Cake, Hiking, OutdoorGrill, SportsScore, SportsFootball, SportsGolf, SportsBasketball, Flight, Hotel, ShoppingCart, Pets, Agriculture, HomeWork, Home, PestControl, AcUnit, Plumbing, PersonalVideo, Healing } from '@styled-icons/material'
 import PropTypes from 'prop-types';
 
 let size=40
 
 let iconObj = {
-    commute: <material.Commute size={size} title="Commute" />,
+    Commute: <Commute size={size} title="Commute" />,
     
-    Business: <material.Business size={size} title="Bussiness Expense" />,
+    Business: <Business size={size} title="Bussiness Expense" />,
     
-    Book: <material.Book size={size} title="Book" />,
+    Book: <Book size={size} title="Book" />,
     
-    Theaters: <material.Theaters size={size} title="Theaters" />,
+    Theaters: <Theaters size={size} title="Theaters" />,
     
-    SportsEsports: <material.SportsEsports size={size} title="Computer" />,
+    SportsEsports: <SportsEsports size={size} title="Computer" />,
     
-    Cake: <material.Cake size={size} title="Birthday" />,
+    Cake: <Cake size={size} title="Birthday" />,
     
-    Hiking: <material.Hiking size={size} title="Hiking" />,
+    Hiking: <Hiking size={size} title="Hiking" />,
     
-    OutdoorGrill: <material.OutdoorGrill size={size} title="Grilling" />,
+    OutdoorGrill: <OutdoorGrill size={size} title="Grilling" />,
     
-    SportsScore: <material.SportsScore size={size} title="General Sports" />,
+    SportsScore: <SportsScore size={size} title="General Sports" />,
     
-    SportsFootball: <material.SportsFootball size={size} title="Football" />,
+    SportsFootball: <SportsFootball size={size} title="Football" />,
     
-    SportsGolf: <material.SportsGolf size={size} title="Golf" />,
+    SportsGolf: <SportsGolf size={size} title="Golf" />,
     
-    SportsBasketball: <material.SportsBasketball size={size} title="Basketball" />,
+    SportsBasketball: <SportsBasketball size={size} title="Basketball" />,
     
-    Flight: <material.Flight size={size} title="Flights" />,
+    Flight: <Flight size={size} title="Flights" />,
     
-    Hotel:<material.Hotel size={size} title="Hotel" />,
+    Hotel:<Hotel size={size} title="Hotel" />,
     
-    ShoppingCart: <material.ShoppingCart size={size} title="Groceries" />,
+    ShoppingCart: <ShoppingCart size={size} title="Groceries" />,
     
-    Pets: <material.Pets size={size} title="Pets"/>,
+    Pets: <Pets size={size} title="Pets"/>,
     
-    Agriculture: <material.Agriculture size={size} title="Vegetables" />,
+    Agriculture: <Agriculture size={size} title="Vegetables" />,
     
-    HomeWork: <material.HomeWork size={size} title="Homework" />,
+    HomeWork: <HomeWork size={size} title="Homework" />,
     
-    Home: <material.Home size={size} title="Rent" />,
+    Home: <Home size={size} title="Rent" />,
     
-    PestControl: <material.PestControl size={size} title="Pest Control" />,
+    PestControl: <PestControl size={size} title="Pest Control" />,
     
-    AcUnit: <material.AcUnit size={size} title="AC" />,
+    AcUnit: <AcUnit size={size} title="AC" />,
     
-    Plumbing: <material.Plumbing size={size} title="Water" />,
+    Plumbing: <Plumbing size={size} title="Water" />,
     
-    PersonalVideo: <material.PersonalVideo size={size} title="Camera" />,
+    PersonalVideo: <PersonalVideo size={size} title="Camera" />,
     
-    Healing: <material.Healing size={size} title="Medical Expenses" />,
+    Healing: <Healing size={size} title="Medical Expenses" />,
 
 }
 
 
 const Icon = (props) => {
+    console.log(props.name)
 
     const DynamicIcon = iconObj[props.name]
   
+    // const MaterialIcon = (name) => {
+    //     let resolved = require(`@styled-icons/material/${name}`).default
+
+    //     console.log(resolved)
+        
+    //     if (!resolved) {
+    //         throw Error(`Could not find ${name}`)
+    //     }
+    
+    //     return React.createElement(resolved)
+    // }
+
+    // return <MaterialIcon name={props.name}/>
+
     return (
+
+
     //   <Suspense fallback={<div>Loading ...</div>}>
     //     <DynamicIcon />
     //   </Suspense>
